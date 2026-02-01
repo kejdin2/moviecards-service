118257a080c88761ae296fc88f95a31f92fe481e
Kejdi Neziri <kejdineziri2@gmail.com>
2026-02-01 18:33:40 +0100

    Initial moviecards-service REST backend

--- src/main/java/com/lauracercas/moviecards/controller/ActorController.java
+++ src/main/java/com/lauracercas/moviecards/controller/ActorController.java
@@ -1,26 +1,14 @@
 package com.lauracercas.moviecards.controller;
 
 import com.lauracercas.moviecards.model.Actor;
-import com.lauracercas.moviecards.model.Movie;
 import com.lauracercas.moviecards.service.actor.ActorService;
-import com.lauracercas.moviecards.util.Messages;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-/**
- * Autor: Laura Cercas Ramos
- * Proyecto: TFM Integración Continua con GitHub Actions
- * Fecha: 04/06/2024
- */
-@Controller
+@RestController
+@RequestMapping("/api/actors")
 public class ActorController {
 
     private final ActorService actorService;
@@ -29,47 +17,18 @@ public class ActorController {
         this.actorService = actorService;
     }
 
-    @GetMapping("actors")
-    public String getActorsList(Model model) {
-        model.addAttribute("actors", actorService.getAllActors());
-        return "actors/list";
+    @GetMapping
+    public List<Actor> getAllActors() {
+        return actorService.getAllActors();
     }
 
-    @GetMapping("actors/new")
-    public String newActor(Model model) {
-        model.addAttribute("actor", new Actor());
-        model.addAttribute("title", Messages.NEW_ACTOR_TITLE);
-        return "actors/form";
+    @GetMapping("/{id}")
+    public Actor getActor(@PathVariable Integer id) {
+        return actorService.getActorById(id);
     }
 
-    @PostMapping("saveActor")
-    public String saveActor(@ModelAttribute Actor actor, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "actors/form";
-        }
-        Actor actorSaved = actorService.save(actor);
-        if (actor.getId() != null) {
-            model.addAttribute("message", Messages.UPDATED_ACTOR_SUCCESS);
-        } else {
-            model.addAttribute("message", Messages.SAVED_ACTOR_SUCCESS);
-        }
-
-        model.addAttribute("actor", actorSaved);
-        model.addAttribute("title", Messages.EDIT_ACTOR_TITLE);
-        return "actors/form";
+    @PostMapping
+    public Actor saveActor(@RequestBody Actor actor) {
+        return actorService.save(actor);
     }
-
-    @GetMapping("editActor/{actorId}")
-    public String editActor(@PathVariable Integer actorId, Model model) {
-        Actor actor = actorService.getActorById(actorId);
-        List<Movie> movies = actor.getMovies();
-        model.addAttribute("actor", actor);
-        model.addAttribute("movies", movies);
-
-        model.addAttribute("title", Messages.EDIT_ACTOR_TITLE);
-
-        return "actors/form";
-    }
-
-
 }
